eed61f2ba09899f81203e0ec568d26871fd7cd30
Andres Contreras <andres_c8@outlook.com>
2020-02-29 21:30:05 -0500

    archivos iniciales

---
+++ src/main/java/springsoaptorest/springusercards/SpringUserCardsApplication.java
@@ -0,0 +1,13 @@
+package springsoaptorest.springusercards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUserCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springsoaptorest/springusercards/SpringUserCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package springsoaptorest.springusercards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
