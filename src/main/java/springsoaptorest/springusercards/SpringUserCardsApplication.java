9d29bdc9af83bc2cc4f6150da2e00f0da1b15b14
Andres Contreras <andres_c8@outlook.com>
2020-03-02 01:56:48 -0500

    preparar soap

--- src/main/java/springsoaptorest/springusercards/SpringUserCardsApplication.java
+++ src/main/java/springsoaptorest/springusercards/SpringUserCardsApplication.java
@@ -8,6 +8,32 @@ public class SpringUserCardsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringUserCardsApplication.class, args);
+		
+
+// https://howtodoinjava.com/spring-boot/spring-soap-client-webservicetemplate/
+		
+//		<plugin>
+//		<groupId>org.jvnet.jaxb2.maven2</groupId>
+//		<artifactId>maven-jaxb2-plugin</artifactId>
+//		<version>0.13.2</version>
+//		<executions>
+//			<execution>
+//				<goals>
+//					<goal>generate</goal>
+//				</goals>
+//			</execution>
+//		</executions>
+//		<configuration>
+//			<generatePackage>springsoaptorest.springusercards.schemas.checkoutwsweb</generatePackage>
+//			<generateDirectory>${project.basedir}/src/main/java</generateDirectory>
+//			<schemaDirectory>${project.basedir}/src/main/resources/wsdl</schemaDirectory>
+//			<schemaIncludes>
+//				<include>*.wsdl</include>
+//			</schemaIncludes>
+//			<forceRegenerate>true</forceRegenerate>
+//		</configuration>
+//	</plugin>
+	
 	}
 
 }
